479cf8124d386d81da6fbbd6da87f40fac0044af
szutumcs50 <ludioludius0@gmail.com>
2018-07-25 22:57:45 +0200

    Add some layouts and test some things

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, request, render_template
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+import requests
+import json
 
+
+#key: vhneXzU3MarEL32SDgrw
 app = Flask(__name__)
 
 # Check for environment variable
@@ -20,7 +24,19 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+#import requests
+#res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "vhneXzU3MarEL32SDgrw", "isbns": "9781632168146"})
+#print(res.json())
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == 'POST':
+        login = request.form['login']
+        print(login)
+        password = request.form['password']
+        print(f"Login:{login} Password:{password}")
+    return render_template("register.html")
